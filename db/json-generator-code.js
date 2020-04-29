ab0246d4024916d9e38833df8c31182f9a932623
gm-david <cuenta.rsd.job@gmail.com>
2020-04-29 02:13:35 -0500

    Nueva Base de Datos

---
+++ db/json-generator-code.js
@@ -0,0 +1,12 @@
+/* https://www.json-generator.com/ */
+[
+    '{{repeat(1000)}}',
+    {
+        title: '{{firstName()}}',
+        created: '{{date(new Date(2014, 0, 1), new Date(), "YYYY-MM-dd hh:mm:ss")}}',
+        description: function (tags) {
+            var fruits = ['apple', 'banana', 'strawberry', 'melon', 'grapes', 'watermelon', 'orange'];
+            return fruits[tags.integer(0, fruits.length - 1)];
+        }
+    }
+]
