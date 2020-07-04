7fa44e1aa46ffc64c01d63a895ef6ca83920a1f0
gm-david <cuenta.rsd.job@gmail.com>
2020-07-04 12:27:59 -0500

    Carpeta "files"

--- functions/functions.php
+++ functions/functions.php
@@ -4,7 +4,7 @@ function almacenarArchivo($name)
 {
     $temporal_name = $_FILES["$name"]['tmp_name'];
     $name = $_FILES["$name"]['name'];
-    $url = "upload/$name";
+    $url = "files/$name";
 
     move_uploaded_file($temporal_name, "$url");
 
